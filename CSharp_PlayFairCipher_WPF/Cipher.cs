1055adb84334d7cada2b6c9cbb78270de5d9a04a
Marek Olšák <m_olsak@utb.cz>
2021-10-19 03:07:01 +0200

    Add project files.

---
+++ CSharp_PlayFairCipher_WPF/Cipher.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using CSharp_PlayFairCipher_WPF.Annotations;
+
+namespace CSharp_PlayFairCipher_WPF
+{
+    public class Cipher : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string KeyWord { get; set; }
+
+        public Cipher()
+        {
+            KeyWord = String.Empty;
+        }
+
+        //[NotifyPropertyChangedInvocator]
+        //protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        //}
+    }
+}
